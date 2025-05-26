48040031817b40f3fbe480bf1b43cc8ef993eff0
Shivam Chaurasiya <shivamchaurasiyaofficial2002@gmail.com>
2025-05-27 03:09:24 +0530

    Just update few thing

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import GoogleButton from "react-google-button";
 
-export const Route = createFileRoute('/login')({
+export const Route = createFileRoute("/login")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:5000/auth/google'
+    console.log("Login button clicked");
+  }
+  return (
+    <>
+      <div className="App-header">
+        <GoogleButton
+          onClick= {handleLogin}
+        />
+      </div>
+    </>
+  );
 }
